edae6efb364dfc8fa4ffe2573374a83caa05c79d
«Andrey_Istomin» <an.istomin@gmail.com>
2024-10-27 12:53:11 +0500

    testing HW11

--- src/main/java/ru/geekbrains/springHW5/controllers/TaskController.java
+++ src/main/java/ru/geekbrains/springHW5/controllers/TaskController.java
@@ -15,53 +15,34 @@ import java.util.List;
 public class TaskController {
     private final TaskService taskService;
 
-    /**
-     * Добавление задачи в список
-     * @param task сама задача
-     * @return  возвращаем новую задачу
-     */
+
     @PostMapping
     public Task addTask(@RequestBody Task task){
         task.setCreatedDate(LocalDateTime.now());
         return taskService.addTask(task);
     }
 
-    /**
-     * Получение списка всех задач.
-     * @return возвращаем список всех задач.
-     */
+
     @GetMapping
     public List<Task> getAllTasks() {
         return taskService.getAllTasks();
     }
 
 
-    /**
-     * Просмотр задач по статусу
-     * @param status    статус задач
-     * @return список задач одного статуса
-     */
+
     @GetMapping("/status/{status}")
     public List<Task> findByStatus(@PathVariable TaskStatus status){
         return taskService.findByStatus(status);
     }
 
 
-    /**
-     * Обновление статуса задачи по её id
-     * @param id    id задачи
-     * @param task сама задача
-     * @return  вернется обновленная задача с новым статусом.
-     */
+
     @PutMapping("/{id}")
     public Task updateTaskStatus(@PathVariable Long id, @RequestBody Task task){
         return taskService.updateTaskStatus(id,task);
     }
 
-    /**
-     * Удаление задачи по её ID
-     * @param id ID задачи
-     */
+
     @DeleteMapping("/{id}")
     public void deleteTask(@PathVariable Long id){
         taskService.deleteTask(id);
